238473c0c3c9eb28d3cdc173221d185c3f7a83c4
abecombe <abecombe240@yahoo.co.jp>
2023-06-02 19:11:05 +0900

    fix gridparamssampling

--- Assets/ComputeShaders/Simulation/GridParamsSampling.hlsl
+++ Assets/ComputeShaders/Simulation/GridParamsSampling.hlsl
@@ -9,36 +9,40 @@
 #define S0(S) (1.0f - S)
 #define S1(S) (S)
 
-#define INTERPOLATE_X(SUM, C_INDEX, S, GRID_BUFFER, AXIS) {\
+#define INTERPOLATE_X(SUM, C_INDEX, S, WEIGHT, GRID_BUFFER, STRUCT) {\
 const uint c_id_0 = CellIndexToCellID(C_INDEX + int3(0, 0, 0));\
 const uint c_id_1 = CellIndexToCellID(C_INDEX + int3(1, 0, 0));\
-SUM += GRID_BUFFER[c_id_0].AXIS * S0(S.x);\
-SUM += GRID_BUFFER[c_id_1].AXIS * S1(S.x);\
+STRUCT sum_x = (STRUCT)0;\
+sum_x += GRID_BUFFER[c_id_0] * S0(S.x);\
+sum_x += GRID_BUFFER[c_id_1] * S1(S.x);\
+sum_x *= WEIGHT;\
+SUM += sum_x;\
 }\
 
-#define INTERPOLATE_Y(SUM, C_INDEX, S, GRID_BUFFER, AXIS) {\
-float2 sum_x = (float2)0;\
-INTERPOLATE_X(sum_x[0], C_INDEX + int3(0, 0, 0), S, GRID_BUFFER, AXIS)\
-INTERPOLATE_X(sum_x[1], C_INDEX + int3(0, 1, 0), S, GRID_BUFFER, AXIS)\
-SUM += sum_x[0] * S0(S.y);\
-SUM += sum_x[1] * S1(S.y);\
+#define INTERPOLATE_Y(SUM, C_INDEX, S, WEIGHT, GRID_BUFFER, STRUCT) {\
+STRUCT sum_y = (STRUCT)0;\
+INTERPOLATE_X(sum_y, C_INDEX + int3(0, 0, 0), S, S0(S.y), GRID_BUFFER, STRUCT)\
+INTERPOLATE_X(sum_y, C_INDEX + int3(0, 1, 0), S, S1(S.y), GRID_BUFFER, STRUCT)\
+sum_y *= WEIGHT;\
+SUM += sum_y;\
 }\
 
-#define INTERPOLATE_Z(SUM, C_INDEX, S, GRID_BUFFER, AXIS) {\
-float2 sum_y = (float2)0;\
-INTERPOLATE_Y(sum_y[0], C_INDEX + int3(0, 0, 0), S, GRID_BUFFER, AXIS)\
-INTERPOLATE_Y(sum_y[1], C_INDEX + int3(0, 0, 1), S, GRID_BUFFER, AXIS)\
-SUM += sum_y[0] * S0(S.z);\
-SUM += sum_y[1] * S1(S.z);\
+#define INTERPOLATE_Z(SUM, C_INDEX, S, GRID_BUFFER, STRUCT) {\
+INTERPOLATE_Y(SUM, C_INDEX + int3(0, 0, 0), S, S0(S.z), GRID_BUFFER, STRUCT)\
+INTERPOLATE_Y(SUM, C_INDEX + int3(0, 0, 1), S, S1(S.z), GRID_BUFFER, STRUCT)\
 }\
 
-#define INTERPOLATE(VALUE, C_INDEX, S, GRID_BUFFER, AXIS) \
-INTERPOLATE_Z(VALUE, C_INDEX, S, GRID_BUFFER, AXIS)\
+#define INTERPOLATE(VALUE, C_INDEX, S, GRID_BUFFER, STRUCT) {\
+INTERPOLATE_Z(VALUE, C_INDEX, S, GRID_BUFFER, STRUCT)\
+}\
 
-#define SAMPLE_GRID_PARAM(VALUE, G_POS, GRID_BUFFER, AXIS) \
-const int3 c_index = floor(G_POS - 0.5f);\
-const float3 s = frac(G_POS - 0.5f);\
-INTERPOLATE(VALUE, c_index, s, GRID_BUFFER, AXIS)\
+#define SAMPLE_GRID_PARAM(POS, GRID_BUFFER, STRUCT) \
+STRUCT ret = 0;\
+const float3 g_pos = WorldPosToGridPos(POS);\
+const int3 c_index = floor(g_pos - 0.5f);\
+const float3 s = frac(g_pos - 0.5f);\
+INTERPOLATE(ret, c_index, s, GRID_BUFFER, STRUCT)\
+return ret;\
 
 #elif defined(USE_QUADRATIC_KERNEL)
 // sample grid param using quadratic interpolation
@@ -46,84 +50,83 @@ INTERPOLATE(VALUE, c_index, s, GRID_BUFFER, AXIS)\
 #define S1(S) (0.75f - S * S)
 #define S2(S) (0.5f * (0.5f + S) * (0.5f + S))
 
-#define INTERPOLATE_X(SUM, C_INDEX, S, GRID_BUFFER, AXIS) {\
+#define INTERPOLATE_X(SUM, C_INDEX, S, WEIGHT, GRID_BUFFER, STRUCT) {\
 const uint c_id_0 = CellIndexToCellID(C_INDEX + int3(-1, 0, 0));\
 const uint c_id_1 = CellIndexToCellID(C_INDEX + int3(0, 0, 0));\
 const uint c_id_2 = CellIndexToCellID(C_INDEX + int3(1, 0, 0));\
-SUM += GRID_BUFFER[c_id_0].AXIS * S0(S.x);\
-SUM += GRID_BUFFER[c_id_1].AXIS * S1(S.x);\
-SUM += GRID_BUFFER[c_id_2].AXIS * S2(S.x);\
+STRUCT sum_x = (STRUCT)0;\
+sum_x += GRID_BUFFER[c_id_0] * S0(S.x);\
+sum_x += GRID_BUFFER[c_id_1] * S1(S.x);\
+sum_x += GRID_BUFFER[c_id_2] * S2(S.x);\
+sum_x *= WEIGHT;\
+SUM += sum_x;\
 }\
 
-#define INTERPOLATE_Y(SUM, C_INDEX, S, GRID_BUFFER, AXIS) {\
-float3 sum_x = (float3)0;\
-INTERPOLATE_X(sum_x[0], C_INDEX + int3(0, -1, 0), S, GRID_BUFFER, AXIS)\
-INTERPOLATE_X(sum_x[1], C_INDEX + int3(0, 0, 0), S, GRID_BUFFER, AXIS)\
-INTERPOLATE_X(sum_x[2], C_INDEX + int3(0, 1, 0), S, GRID_BUFFER, AXIS)\
-SUM += sum_x[0] * S0(S.y);\
-SUM += sum_x[1] * S1(S.y);\
-SUM += sum_x[2] * S2(S.y);\
+#define INTERPOLATE_Y(SUM, C_INDEX, S, WEIGHT, GRID_BUFFER, STRUCT) {\
+STRUCT sum_y = (STRUCT)0;\
+INTERPOLATE_X(sum_y, C_INDEX + int3(0, -1, 0), S, S0(S.y), GRID_BUFFER, STRUCT)\
+INTERPOLATE_X(sum_y, C_INDEX + int3(0, 0, 0), S, S1(S.y), GRID_BUFFER, STRUCT)\
+INTERPOLATE_X(sum_y, C_INDEX + int3(0, 1, 0), S, S2(S.y), GRID_BUFFER, STRUCT)\
+sum_y *= WEIGHT;\
+SUM += sum_y;\
 }\
 
-#define INTERPOLATE_Z(SUM, C_INDEX, S, GRID_BUFFER, AXIS) {\
-float3 sum_y = (float3)0;\
-INTERPOLATE_Y(sum_y[0], C_INDEX + int3(0, 0, -1), S, GRID_BUFFER, AXIS)\
-INTERPOLATE_Y(sum_y[1], C_INDEX + int3(0, 0, 0), S, GRID_BUFFER, AXIS)\
-INTERPOLATE_Y(sum_y[2], C_INDEX + int3(0, 0, 1), S, GRID_BUFFER, AXIS)\
-SUM += sum_y[0] * S0(S.z);\
-SUM += sum_y[1] * S1(S.z);\
-SUM += sum_y[2] * S2(S.z);\
+#define INTERPOLATE_Z(SUM, C_INDEX, S, GRID_BUFFER, STRUCT) {\
+INTERPOLATE_Y(SUM, C_INDEX + int3(0, 0, -1), S, S0(S.z), GRID_BUFFER, STRUCT)\
+INTERPOLATE_Y(SUM, C_INDEX + int3(0, 0, 0), S, S1(S.z), GRID_BUFFER, STRUCT)\
+INTERPOLATE_Y(SUM, C_INDEX + int3(0, 0, 1), S, S2(S.z), GRID_BUFFER, STRUCT)\
 }\
 
-#define INTERPOLATE(VALUE, C_INDEX, S, GRID_BUFFER, AXIS) \
-INTERPOLATE_Z(VALUE, C_INDEX, S, GRID_BUFFER, AXIS)\
+#define INTERPOLATE(VALUE, C_INDEX, S, GRID_BUFFER, STRUCT) {\
+INTERPOLATE_Z(VALUE, C_INDEX, S, GRID_BUFFER, STRUCT)\
+}\
 
-#define SAMPLE_GRID_PARAM(VALUE, G_POS, GRID_BUFFER, AXIS) \
-const int3 c_index = round(G_POS - 0.5f);\
-const float3 s = (G_POS - 0.5f) - round(G_POS - 0.5f);\
-INTERPOLATE(VALUE, c_index, s, GRID_BUFFER, AXIS)\
+#define SAMPLE_GRID_PARAM(POS, GRID_BUFFER, STRUCT) \
+STRUCT ret = 0;\
+const float3 g_pos = WorldPosToGridPos(POS);\
+const int3 c_index = round(g_pos - 0.5f);\
+const float3 s = (g_pos - 0.5f) - round(g_pos - 0.5f);\
+INTERPOLATE(ret, c_index, s, GRID_BUFFER, STRUCT)\
+return ret;\
 
 #else
 // default
-#define SAMPLE_GRID_PARAM(VALUE, G_POS, GRID_BUFFER, AXIS) \
-VALUE = 0;\
+#define SAMPLE_GRID_PARAM(POS, GRID_BUFFER, STRUCT) \
+return (STRUCT)0;\
 
 #endif
 
-#define SAMPLE_GRID_SCALAR(VALUE, POS, GRID_BUFFER) {\
-const float3 g_pos = (POS - _GridMin) * _GridInvSpacing;\
-SAMPLE_GRID_PARAM(VALUE, g_pos, GRID_BUFFER, x)\
-}\
-
-#define SAMPLE_GRID_VECTOR_X(VALUE, POS, GRID_BUFFER) {\
-const float3 g_pos = (POS - _GridMin) * _GridInvSpacing;\
-SAMPLE_GRID_PARAM(VALUE, g_pos, GRID_BUFFER, x)\
-}\
-
-#define SAMPLE_GRID_VECTOR_Y(VALUE, POS, GRID_BUFFER) {\
-const float3 g_pos = (POS - _GridMin) * _GridInvSpacing;\
-SAMPLE_GRID_PARAM(VALUE, g_pos, GRID_BUFFER, y)\
-}\
-
-#define SAMPLE_GRID_VECTOR_Z(VALUE, POS, GRID_BUFFER) {\
-const float3 g_pos = (POS - _GridMin) * _GridInvSpacing;\
-SAMPLE_GRID_PARAM(VALUE, g_pos, GRID_BUFFER, z)\
-}\
-
-inline float SampleGridParam(float3 pos, StructuredBuffer<float> grid_buffer)
+inline float SampleGridParam(float3 position, StructuredBuffer<float> grid_buffer)
 {
-    float value = 0;
-    SAMPLE_GRID_SCALAR(value, pos, grid_buffer)
-    return value;
+    SAMPLE_GRID_PARAM(position, grid_buffer, float)
 }
-
-inline float3 SampleGridParam(float3 pos, StructuredBuffer<float3> grid_buffer)
+inline float SampleGridParam(float3 position, RWStructuredBuffer<float> grid_buffer)
 {
-    float3 value = 0;
-    SAMPLE_GRID_VECTOR_X(value.x, pos, grid_buffer)
-    SAMPLE_GRID_VECTOR_Y(value.y, pos, grid_buffer)
-    SAMPLE_GRID_VECTOR_Z(value.z, pos, grid_buffer)
-    return value;
+    SAMPLE_GRID_PARAM(position, grid_buffer, float)
+}
+inline float2 SampleGridParam(float3 position, StructuredBuffer<float2> grid_buffer)
+{
+    SAMPLE_GRID_PARAM(position, grid_buffer, float2)
+}
+inline float2 SampleGridParam(float3 position, RWStructuredBuffer<float2> grid_buffer)
+{
+    SAMPLE_GRID_PARAM(position, grid_buffer, float2)
+}
+inline float3 SampleGridParam(float3 position, StructuredBuffer<float3> grid_buffer)
+{
+    SAMPLE_GRID_PARAM(position, grid_buffer, float3)
+}
+inline float3 SampleGridParam(float3 position, RWStructuredBuffer<float3> grid_buffer)
+{
+    SAMPLE_GRID_PARAM(position, grid_buffer, float3)
+}
+inline float4 SampleGridParam(float3 position, StructuredBuffer<float4> grid_buffer)
+{
+    SAMPLE_GRID_PARAM(position, grid_buffer, float4)
+}
+inline float4 SampleGridParam(float3 position, RWStructuredBuffer<float4> grid_buffer)
+{
+    SAMPLE_GRID_PARAM(position, grid_buffer, float4)
 }
 
 
